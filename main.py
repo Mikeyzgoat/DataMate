8c464b61043e5bbad05653233c76bb2a535c41fa
Mikeyzgoat <tadisai091@gmail.com>
2024-05-22 17:12:30 +0530

    Added base classes

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import numpy as np
 import pandas as pd
-import fastapi
-from fastapi import FastAPI, 
+import nltk
+from fastapi import FastAPI,UploadFile,File
+from pydantic import BaseModel
+
+class FileHandler(BaseModel):
+    file: UploadFile
+
+class TextCleaner():
+    pass
+
+class PDFHandler():
+    pass
+
+class CSVHandler():
+    pass
+
+class WebDataHandler():
+    # web scraper based on URL passed into form or from csv files
+    pass
